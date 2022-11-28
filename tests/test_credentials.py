b5f056f0446202e86d0d6c313e58a0f8f1d9dbe0
Serina Grill <serinagrill@gmail.com>
2022-11-28 16:54:31 -0600

    Add more test params

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -4,7 +4,9 @@ from prefect.blocks.core import Block
 from prefect_bitbucket import BitBucketCredentials
 
 
-@pytest.mark.parametrize("token", [None, "token_value"])
+@pytest.mark.parametrize(
+    "token", [None, "token_value", "invalid!token", "tokenthatisoverthirtycharacters"]
+)
 def test_bitbucket_credentials_get_endpoint(token):
     credentials_block = BitBucketCredentials(token=token)
     assert isinstance(credentials_block, Block)
