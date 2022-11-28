1ad89ecfe82d68bf8a365138f0c12ca06d02fedf
Serina Grill <serinagrill@gmail.com>
2022-11-28 16:40:31 -0600

    Add test_credentials

---
+++ tests/test_credentials.py
@@ -0,0 +1,10 @@
+import pytest
+from prefect.blocks.core import Block
+
+from prefect_bitbucket import BitBucketCredentials
+
+
+@pytest.mark.parametrize("token", [None, "token_value"])
+def test_github_credentials_get_endpoint(token):
+    credentials_block = BitBucketCredentials(token=token)
+    assert isinstance(credentials_block, Block)
