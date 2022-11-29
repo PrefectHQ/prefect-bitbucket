c5be0254f2c364e631e27bb8fc86356e131d69e5
Serina Grill <serinagrill@gmail.com>
2022-11-29 12:43:29 -0600

    Add docstrings

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -5,17 +5,20 @@ from prefect_bitbucket.credentials import BitBucketCredentials
 
 
 @pytest.mark.parametrize("token", [None, "token_value"])
-def test_bitbucket_credentials_get_endpoint(token):
+def test_bitbucket_credentials(token):
+    """Test credentials is Block type."""
     credentials_block = BitBucketCredentials(token=token)
     assert isinstance(credentials_block, Block)
 
 
 def test_bitbucket_username_invalid_char():
+    """Ensure invalid char username raises."""
     with pytest.raises(ValueError):
         BitBucketCredentials(token="token", username="invalid!username")
 
 
 def test_bitbucket_username_over_max_length():
+    """Ensure username of greater than max allowed length raises."""
     with pytest.raises(ValueError):
         BitBucketCredentials(
             token="token", username="usernamethatisoverthirtycharacters"
