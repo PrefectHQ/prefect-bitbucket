f51f6b7940af9aec0f309d361ec76ebf988189ea
Serina Grill <serinagrill@gmail.com>
2022-11-28 18:43:47 -0600

    add tests

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -1,12 +1,20 @@
 import pytest
 from prefect.blocks.core import Block
 
-from prefect_bitbucket import BitBucketCredentials
+from prefect_bitbucket.credentials import BitBucketCredentials
 
 
-@pytest.mark.parametrize(
-    "token", [None, "token_value", "invalid!token", "tokenthatisoverthirtycharacters"]
-)
+@pytest.mark.parametrize("token", [None, "token_value"])
 def test_bitbucket_credentials_get_endpoint(token):
     credentials_block = BitBucketCredentials(token=token)
     assert isinstance(credentials_block, Block)
+
+
+def test_bitbucket_username_invalid_char():
+    with pytest.raises(ValueError):
+        BitBucketCredentials(token="token", username="invalid!username")
+
+
+def test_bitbucket_username_over_max_length():
+    with pytest.raises(ValueError):
+        BitBucketCredentials(token="token", username="tokenthatisoverthirtycharacters")
