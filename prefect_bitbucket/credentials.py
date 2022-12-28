719791c9954acf9fb99937f241b0f90b9ab275cb
Andrew <15331990+ahuang11@users.noreply.github.com>
2022-12-28 11:59:31 -0800

    Add client (#7)
    * Initial commit
    
    * Add docstring to tof
    
    * Update changelog
    
    * update mkdocs
    
    * Update init with credentials
    
    * update readme with credentials example
    
    * initial commit
    
    * Update imports in examples
    
    * Initial commit
    
    * Update CHANGELOG.md
    
    Co-authored-by: Andrew <15331990+ahuang11@users.noreply.github.com>
    
    * Update README.md
    
    Co-authored-by: Andrew <15331990+ahuang11@users.noreply.github.com>
    
    * Update README.md
    
    Co-authored-by: Andrew <15331990+ahuang11@users.noreply.github.com>
    
    * Update README.md
    
    Co-authored-by: Andrew <15331990+ahuang11@users.noreply.github.com>
    
    * Add creds test
    
    * token -> username
    
    * Add repo block to changelog
    
    * update mkdocs
    
    * Initial commit
    
    * Fix docstrings
    
    * Add repo to init
    
    * Update init
    
    * Remove current chngs
    
    * Add example to top
    
    * Update prefect version to use exceptions
    
    * add pydocstyle
    
    * Add examples to readme
    
    * pydocstyle credentials
    
    * Add docstrings
    
    * Initial commit
    
    * initial commit
    
    * adding d104
    
    * Spelling error
    
    * Update the changelog
    
    * Add periods to examples
    
    * Update CHANGELOG.md
    
    Co-authored-by: Andrew <15331990+ahuang11@users.noreply.github.com>
    
    * Update prefect_bitbucket/repository.py
    
    Co-authored-by: Andrew <15331990+ahuang11@users.noreply.github.com>
    
    * fix comment
    
    * Update README.md
    
    Co-authored-by: Andrew <15331990+ahuang11@users.noreply.github.com>
    
    * Update README.md
    
    Co-authored-by: Andrew <15331990+ahuang11@users.noreply.github.com>
    
    * Update tests/test_repository.py
    
    Co-authored-by: Andrew <15331990+ahuang11@users.noreply.github.com>
    
    * Modify docstring
    
    * Rename credentials to bitbucket_credentials
    
    * Ignore tests in pydocstyle
    
    * Remove all the periods from headings
    
    * Remove 1-
    
    * pydocstyle linting
    
    * removing d212
    
    * satisfy pydocstyle
    
    * Allow D406 D407
    
    * credentials -> bitbucket_credentials
    
    * Added unit test for credentials = None
    
    * Add passing username test
    
    * Add client
    
    * Add changelog
    
    * Linting
    
    * Add docstring
    
    * Add reqs
    
    * Update CHANGELOG.md
    
    * Address comments
    
    * Docstring
    
    * Update prefect_bitbucket/credentials.py

--- prefect_bitbucket/credentials.py
+++ prefect_bitbucket/credentials.py
@@ -1,17 +1,32 @@
 """Module to enable authenticate interactions with BitBucket."""
 import re
-from typing import Optional
+from enum import Enum
+from typing import Optional, Union
 
-from prefect.blocks.core import Block
+from prefect.blocks.abstract import CredentialsBlock
 from pydantic import Field, SecretStr, validator
 
+try:
+    from atlassian.bitbucket import Bitbucket, Cloud
+except ImportError:
+    pass
 
-class BitBucketCredentials(Block):
+
+class ClientType(Enum):
+    """The client type to use."""
+
+    LOCAL = "local"
+    CLOUD = "cloud"
+
+
+class BitBucketCredentials(CredentialsBlock):
     """Store BitBucket credentials to interact with private BitBucket repositories.
 
     Attributes:
         token: An access token to authenticate with BitBucket.
         username: Identification name unique across entire BitBucket site.
+        password: The password to authenticate to BitBucket.
+        url: The base URL of your BitBucket instance.
 
 
     Examples:
@@ -26,7 +41,7 @@ class BitBucketCredentials(Block):
 
     _block_type_name = "BitBucket Credentials"
     _logo_url = "https://images.ctfassets.net/gm98wzqotmnx/27LMR24ewTSDW238Lks1vH/34c5028659f4007528feadc8db8cecbd/500px-Bitbucket-blue-logomark-only.svg.png?h=250"  # noqa
-    token: SecretStr = Field(
+    token: Optional[SecretStr] = Field(
         name="Personal Access Token",
         default=None,
         description="A BitBucket Personal Access Token.",
@@ -34,6 +49,14 @@ class BitBucketCredentials(Block):
     username: Optional[str] = Field(
         default=None,
         description="Identification name unique across entire BitBucket site.",
+    )
+    password: Optional[SecretStr] = Field(
+        default=None, description="The password to authenticate to BitBucket."
+    )
+    url: str = Field(
+        default="https://api.bitbucket.org/",
+        description="The base URL of your BitBucket instance.",
+        title="URL",
     )
 
     @validator("username")
@@ -48,3 +71,31 @@ class BitBucketCredentials(Block):
         if not len(value) <= 30:
             raise ValueError("Username cannot be longer than 30 chars.")
         return value
+
+    def get_client(
+        self, client_type: Union[str, ClientType], **client_kwargs
+    ) -> Union[Cloud, Bitbucket]:
+        """Get an authenticated local or cloud Bitbucket client.
+
+        Args:
+            client_type: Whether to use a local or cloud client.
+
+        Returns:
+            An authenticated Bitbucket client.
+
+        """
+        # ref: https://atlassian-python-api.readthedocs.io/
+        if isinstance(client_type, str):
+            client_type = ClientType(client_type.lower())
+
+        password = self.password.get_secret_value()
+        input_client_kwargs = dict(
+            url=self.url, username=self.username, password=password
+        )
+        input_client_kwargs.update(**client_kwargs)
+
+        if client_type == ClientType.CLOUD:
+            client = Cloud(**input_client_kwargs)
+        else:
+            client = Bitbucket(**input_client_kwargs)
+        return client
--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -1,7 +1,8 @@
 import pytest
+from atlassian.bitbucket import Bitbucket, Cloud
 from prefect.blocks.core import Block
 
-from prefect_bitbucket.credentials import BitBucketCredentials
+from prefect_bitbucket.credentials import BitBucketCredentials, ClientType
 
 
 @pytest.mark.parametrize("token", [None, "token_value"])
@@ -31,3 +32,21 @@ def test_bitbucket_username_over_max_length():
         BitBucketCredentials(
             token="token", username="usernamethatisoverthirtycharacters"
         )
+
+
+@pytest.mark.parametrize(
+    "client_type",
+    ["local", "LOCAL", "cloud", "Cloud", ClientType.LOCAL, ClientType.CLOUD],
+)
+def test_bitbucket_get_client(client_type):
+    bitbucket_credentials = BitBucketCredentials(
+        url="my-url", username="my-username", password="my-password"
+    )
+    client = bitbucket_credentials.get_client(client_type=client_type)
+    if not isinstance(client_type, str):
+        client_type = client_type.value
+
+    if client_type.lower() == "local":
+        assert isinstance(client, Bitbucket)
+    else:
+        assert isinstance(client, Cloud)
